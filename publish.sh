0b9d55c24da183e5d7dba7f1a2016db7af0ef711
Harish Soni <harish.soni@coditas.com>
2023-05-11 13:53:04 +0530

    Adding the automated publish and deployment script

--- publish.sh
+++ publish.sh
@@ -1,5 +1,28 @@
-echo "Hello, please enter your commit message ?"
+echo "Have you staged/added the changes to commit and publish?"
 
-read cMsg
+read ans
 
-git commit -m "cMsg"
+
+if [[ $and -eq "y" ]]
+ then
+    echo "Hello, please enter your commit message ?"
+
+    read cMsg
+
+    git commit -m "$cMsg"
+
+    echo "Do you want to publish now?"
+
+    read pub
+
+    if [[ &pub -eq "y" ]]
+    then
+        echo "Have you changed the Package Version in package.json?"
+        read change
+
+        if [[ $change -eq "y" ]]
+        then
+            echo "Starting the publish the code...!!!"
+
+            npm run build:npm && npm publish
+fi
