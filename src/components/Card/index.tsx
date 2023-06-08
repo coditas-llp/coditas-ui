571476414ed11eeeab66785df5d613dcecd6d67d
chetanp-coditas <chetan.patil@coditas.com>
2023-06-08 11:10:59 +0530

    Revert "fix: update styles of card component"
    This reverts commit 39491620991a7a472561e59d74999a4bcfdcb5b8.

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,31 +1,29 @@
+import React from 'react'
 import './Card.scss'
 import { useNavigate } from 'react-router-dom'
-import star_bullet_icon from './star_bullet_icon.png';
 
 interface ICardProps {
-  icon?: string
-  heading?: string
-  paragraph?: string
-  more_label?: string
-  buttonRoute?: string
+  icon: string
+  heading: string
+  paragraph: string
+  more_label: string
+  buttonRoute: string
 }
 
 const Card = (props: ICardProps) => {
-  const history = useNavigate();
+  const history = useNavigate()
   return (
     <div className='card'>
-      <div className='image-container'>
-        <img src={props.icon} alt='service-icon' />
-      </div>
+      <img src={props.icon} alt='service-icon' />
       <h3>{props.heading}</h3>
       <div className='description'>
-        {props.paragraph && props.paragraph.split('||').map((text) => (
-          <div className='single-description'>
-            <img src={star_bullet_icon} alt='start bullet icon' className='bullet-point' />
-            <span>{text}</span>
-          </div>
+        {props.paragraph.split('||').map((text) => (
+          <p>{text}</p>
         ))}
       </div>
+      <button onClick={() => history(`${props.buttonRoute}`)}>
+        {props.more_label}
+      </button>
     </div>
   )
 }
